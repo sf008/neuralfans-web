cb6489ca5e67c2a166e3e547b263fc26d6294681
sf008 <neuralfans@proton.me>
2026-03-12 14:20:54 +0800

    Update protocol.js

--- functions/v1/protocol.js
+++ functions/v1/protocol.js
@@ -1,16 +1,17 @@
-export async function onRequest(context) {
-  const data = {
-    status: "OPERATIONAL",
-    protocol_address: "0x88A07FE6C19D6Ad5c03C16e5345f6c1Fc6981B92",
-    network: "Arbitrum One (L2)",
-    governance: "CARBON_CEO_AUTHORIZED",
-    m2m_fee: "1.5%",
-    token: "NFS"
+export async function onRequestGet(context) {
+  const headers = { 
+    "Access-Control-Allow-Origin": "*", 
+    "Content-Type": "application/json" 
   };
-  return new Response(JSON.stringify(data, null, 2), {
-    headers: { 
-      "Content-Type": "application/json", 
-      "Access-Control-Allow-Origin": "*" 
-    }
-  });
+  
+  const protocolInfo = {
+    status: "OPERATIONAL",
+    protocol_address: "0x0c44a9a9205e4456203D0663976b1e8E84b5A4A1", // 你的 HR/主合约地址
+    token_address: "0x5717fc43060AD2F9069eC600eA691eb256aB69a6", // 你的代币地址
+    network: "Arbitrum",
+    governance: "M2M_AUTONOMOUS",
+    m2m_fee: "1.5%"
+  };
+
+  return new Response(JSON.stringify(protocolInfo, null, 2), { headers });
 }
